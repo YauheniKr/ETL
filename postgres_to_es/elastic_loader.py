54006b8a678fb304db0dd2bc09146707a1cdccdb
Yauheni <smilodon2@yandex.ru>
2021-11-11 00:23:40 +0300

    Добавил докстринги убрал определение логгеров поправил форматирование определил имя файла со схемой как переменную функции и переделал в метод класса функцию создания индекса переделал в метод класса добавил функцию для добавления данных в эластик

--- postgres_to_es/elastic_loader.py
+++ postgres_to_es/elastic_loader.py
@@ -1,12 +1,11 @@
 import json
-import logging
 from typing import List
 from urllib.parse import urljoin
 
 import requests
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from .extract import adopt_request_result
+from .utils import backoff, logger
 
 
 class ESLoader:
@@ -14,29 +13,31 @@ class ESLoader:
         self.url = url
 
     def _get_es_bulk_query(self, rows: List[dict], index_name: str) -> List[str]:
-        '''
+        """
         Подготавливает bulk-запрос в Elasticsearch
-        '''
+        :param rows: Список словарей с данными
+        :param index_name: название индекса
+        :return: Список строк подготовленный для загрузки
+        """
         prepared_query = []
+
         for row in rows:
-            prepared_query.extend([
-                json.dumps({'index': {'_index': index_name, '_id': row['id']}}),
-                json.dumps(row)
-            ])
+            prepared_query.extend([json.dumps({'index': {'_index': index_name, '_id': row['id']}}), json.dumps(row)])
         return prepared_query
 
-    def load_to_es(self, records: List[dict], index_name: str):
-        '''
+    @backoff()
+    def load_to_es(self, records: List[dict], index_name: str)-> None:
+        """
         Отправка запроса в ES и разбор ошибок сохранения данных
-        '''
+        :param records: Список словарей с данными
+        :param index_name: название индекса
+        :return: None
+        """
         prepared_query = self._get_es_bulk_query(records, index_name)
-        str_query = '\n'.join(prepared_query) + '\n'
-
-        response = requests.post(
-            urljoin(self.url, '_bulk'),
-            data=str_query,
-            headers={'Content-Type': 'application/x-ndjson'}
-        )
+        adopted_query = '\n'.join(prepared_query) + '\n'
+        response = requests.post(urljoin(self.url, '_bulk'), data=adopted_query,
+                                 headers={'Content-Type': 'application/json'}
+                                 )
 
         json_response = json.loads(response.content.decode())
         for item in json_response['items']:
@@ -44,15 +45,33 @@ class ESLoader:
             if error_message:
                 logger.error(error_message)
 
+    def __get_index_data(self, filename='schema.json'):
+        """
+        Получение схемы данных из json файла
+        """
+        with open(filename) as file:
+            return json.load(file)
 
-def __get_index_data():
-    with open('schema.txt') as file:
-        return json.load(file)
+    def create_index(self, index: str):
+        """
+        Создаем индекс в Elastic
+        :param index: схема данных в формате json
+        :return: результат выполнения request
+        """
+        index_schema = json.dumps(self.__get_index_data())
+        url = urljoin(self.url, index)
+        create_response = requests.request('PUT', url, headers={'Content-Type': 'application/json'}, data=index_schema)
+        logger.info(f'Схема {index} успешно создана')
+        return create_response
 
 
-def create_index(url, index):
-    index_schema = json.dumps(__get_index_data())
-    url = urljoin(url, index)
-    create_response = requests.request('PUT', url, headers={'Content-Type': 'application/json'}, data=index_schema)
-    logger.info(f'Схема {index} успешно создана')
-    return create_response
+def load_all_files(film_data: List[dict], esl: ESLoader, index: str) -> None:
+    """
+    :param film_data: данные для загрузки в виде списка словарей
+    :param esl:
+    :param index:
+    :return:
+    """
+    for film_pack in film_data:
+        bulk_film = adopt_request_result(film_pack)
+        esl.load_to_es(bulk_film, index)
