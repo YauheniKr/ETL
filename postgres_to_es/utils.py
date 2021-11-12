b28c4b34d83bd19a2270c22f870d264e35951792
Yauheni <smilodon2@yandex.ru>
2021-11-12 22:40:12 +0300

    добавил докстринги там где их не хватало

--- postgres_to_es/utils.py
+++ postgres_to_es/utils.py
@@ -26,6 +26,11 @@ class JsonFileStorage(BaseStorage):
         self.file_path = file_path
 
     def save_state(self, state: dict):
+        """
+        Сохраняем полученное состояние
+        :param state: словарь состояния
+        :return:
+        """
         with open(self.file_path, 'w') as file:
             json.dump(state, file, indent=4)
 
@@ -64,6 +69,18 @@ class State:
 
 
 def backoff(logger, start_sleep_time=0.1, factor=2, border_sleep_time=10):
+    """
+    Функция для повторного выполнения функции через некоторое время, если возникла ошибка. Использует наивный экспоненциальный рост времени повтора (factor) до граничного времени ожидания (border_sleep_time)
+
+    Формула:
+        t = start_sleep_time * 2^(n) if t < border_sleep_time
+        t = border_sleep_time if t >= border_sleep_time
+    :param logger: экземпляр логгера для логгирования
+    :param start_sleep_time: начальное время повтора
+    :param factor: во сколько раз нужно увеличить время ожидания
+    :param border_sleep_time: граничное время ожидания
+    :return: результат выполнения функции
+    """
     def real_decorator(func):
         @wraps(func)
         def inner(*args, **kwargs):
