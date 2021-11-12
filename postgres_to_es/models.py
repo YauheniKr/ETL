10bf6ab1e2104f270632efd81261ce0fe056b76e
Yauheni <smilodon2@yandex.ru>
2021-11-12 22:39:25 +0300

    Добавил базовый  класс и классы для хранения списка таблиц для проверки

--- postgres_to_es/models.py
+++ postgres_to_es/models.py
@@ -21,3 +21,27 @@ class Filmwork:
     actors_names: List[str]
     director: List[str]
     genre: List[str]
+
+
+@dataclass(frozen=True)
+class BaseTableClass:
+    table_list: List[str]
+
+    @property
+    def table_to_check_update(self):
+        return self.table_list[0]
+
+
+@dataclass(frozen=True)
+class PersonTables(BaseTableClass):
+    pass
+
+
+@dataclass(frozen=True)
+class GenreTables(BaseTableClass):
+    pass
+
+
+@dataclass(frozen=True)
+class FilmWorkTables(BaseTableClass):
+    pass
